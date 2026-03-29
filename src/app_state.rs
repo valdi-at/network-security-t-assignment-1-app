1651272734ab60c94615dcac21cc4af6c771189d
Mohamad Valdi Ananda Tauhid <valdianandatauhid@proton.me>
2026-03-30 03:12:55 +0700

    refactor: Remove repeating feature declerations

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,26 +1,27 @@
 #[cfg(feature = "ssr")]
-use axum::extract::FromRef;
-use leptos::config::LeptosOptions;
-#[cfg(feature = "ssr")]
-use moka::sync::Cache;
+mod ssr {
+    use axum::extract::FromRef;
+    use leptos::config::LeptosOptions;
+    use moka::sync::Cache;
 
-#[cfg(feature = "ssr")]
-#[derive(Clone)]
-pub struct AppState {
-    pub leptos_options: LeptosOptions,
-    pub sessions: Cache<String, String>,
-}
+    #[derive(Clone)]
+    pub struct AppState {
+        pub leptos_options: LeptosOptions,
+        pub sessions: Cache<String, String>,
+    }
 
-#[cfg(feature = "ssr")]
-impl FromRef<AppState> for LeptosOptions {
-    fn from_ref(state: &AppState) -> Self {
-        state.leptos_options.clone()
+    impl FromRef<AppState> for LeptosOptions {
+        fn from_ref(state: &AppState) -> Self {
+            state.leptos_options.clone()
+        }
+    }
+
+    impl FromRef<AppState> for Cache<String, String> {
+        fn from_ref(state: &AppState) -> Self {
+            state.sessions.clone()
+        }
     }
 }
 
 #[cfg(feature = "ssr")]
-impl FromRef<AppState> for Cache<String, String> {
-    fn from_ref(state: &AppState) -> Self {
-        state.sessions.clone()
-    }
-}
+pub use ssr::*;
